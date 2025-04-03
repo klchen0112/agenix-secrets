d5f0adeda9f04163f2bb49c9fa3cb171795258c9
klchen0112 <klchen0112@gmail.com>
2025-04-03 20:27:42 +0800

    update tokens

--- secrets.nix
+++ secrets.nix
@@ -2,14 +2,14 @@ let
   klchen-3400g = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/c10VIo81cztYJza3e+l1JlwsTJQk1lhBOypGhYn3T";
   klchen-mbp-m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGszCNQqxT1/s6sYjj1aewvCjaa3D7UwoOM7UD5K+ha";
   klchen-sanjiao = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx1SNaQZ6v1onDSGz1wNX1W3zIf2KkTERjKGC+k157D";
-  klchen-mbp-dxm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILre1BWBxY/iL7pnSRIQxVHkQq9b2H5dJTCQvJMMJWpi";
+  klchen-mbp-dxm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILJsUHc8XEf0Pe2acybyO4uEoYu/FrqjX74cYQCuHuR5";
   users = [ klchen-3400g klchen-mbp-m1 klchen-sanjiao klchen-mbp-dxm];
 
   # darwin-
   nixos-3400g = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsw5gk6koAb2D1SYnHt3jhYLNCWChR6eFKJ3vPO3tZY";
   mbp-m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9ZvdIrZP9su70iBKgCB0QOY0kL9Z9qu3B9Of05VS5a";
   sanjiao = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8T9wyRN9CA/wWN70aHaRoAi1BRFeWkIjfL6+sycRaI";
-  mbp-dxm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAoP716OUCUyV56GoCX6dOjiC73lEiB9PFvoS4N3dwAF";
+  mbp-dxm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8NNbMeWmANWXw/oLFRsKPxc8gmMgyhQFYb+v0lrkpI";
   systems = [ nixos-3400g mbp-m1 sanjiao mbp-dxm];
 in
 {
